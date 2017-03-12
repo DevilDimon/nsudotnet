ce940405f6980890ee5424531b315587a7b28457
Серов Дмитрий <barbari100@gmail.com>
2017-03-12 23:11:05 +0700

    basic calendar output

---
+++ Serov.Nsudotnet.Calendar/Calendar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace Serov.Nsudotnet.Calendar {
+    public class Calendar {
+        private DateTime _dateTime;
+
+        public Calendar(DateTime dateTime) {
+            _dateTime = dateTime;
+        }
+
+        public override string ToString() {
+            var sb = new StringBuilder();
+
+            var knownWeekDay = new DateTime(2017, 03, 06);
+            for (var i = 0; i < 7; i++) {
+                sb.AppendFormat("  {0}", knownWeekDay.ToString("ddd").Substring(0, 2));
+                knownWeekDay = knownWeekDay.AddDays(1);
+            }
+            sb.Append(Environment.NewLine);
+
+            var cur = new DateTime(_dateTime.Year, _dateTime.Month, 1);
+            for (var i = 0; i < (cur.DayOfWeek == DayOfWeek.Sunday ? 6 : (int) cur.DayOfWeek - 1); i++) {
+                sb.Append("    ");
+            }
+            while (cur.Month == _dateTime.Month) {
+                sb.AppendFormat("  {0, 2}", cur.Day);
+                if (cur.DayOfWeek == DayOfWeek.Sunday) {
+                    sb.Append(Environment.NewLine);
+                }
+                cur = cur.AddDays(1);
+            }
+            sb.Append(Environment.NewLine);
+
+            return sb.ToString();
+        }
+    }
+}
--- Serov.Nsudotnet.Calendar/Program.cs
+++ Serov.Nsudotnet.Calendar/Program.cs
@@ -1,8 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Serov.Nsudotnet.Calendar {
     internal class Program {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args) {
+            Console.WriteLine("Enter a date:");
+            DateTime dateTime;
+
+            var parsed = DateTime.TryParse(Console.ReadLine(), out dateTime);
+            if (!parsed) {
+                Console.WriteLine("Wrong date format. Terminating...");
+                return;
+            }
+
+            var calendar = new Calendar(dateTime);
+            Console.WriteLine(calendar);
+        }
     }
 }
